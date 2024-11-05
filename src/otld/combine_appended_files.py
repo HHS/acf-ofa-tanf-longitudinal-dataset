bc946ae7c474feebaa069bb9391bd88592c24d84
RGilliard-Arch <150460521+RGilliard-Arch@users.noreply.github.com>
2024-11-05 16:35:09 -0500

    WIP: Begin drafting module to combine appended files

---
+++ src/otld/combine_appended_files.py
@@ -0,0 +1,37 @@
+import os
+
+import pandas as pd
+
+from otld.paths import inter_dir
+
+
+def main():
+    files = os.listdir(inter_dir)
+    federal = []
+    state = []
+    post_2015 = {}
+
+    # Combine files before 2015
+    for file in files:
+        df = pd.read_csv(os.path.join(inter_dir, file))
+        level = "state" if file.startswith("state") else "federal"
+
+        if file.find("2015_2023") > -1:
+            post_2015[level] = df
+        elif level == "federal":
+            federal.append(df)
+        elif level == "state":
+            state.append(df)
+
+    state = pd.concat(state)
+    federal = pd.concat(federal)
+
+    print(state)
+    print(federal)
+
+    # Map columns across 2014/2015 disjunction
+    # Append pre and post-2015
+
+
+if __name__ == "__main__":
+    main()
