669e217849aa654790a70c2a54f4307b7af5ce9a
RGilliard-Arch <150460521+RGilliard-Arch@users.noreply.github.com>
2024-11-19 17:01:57 -0500

    Begin working on linking across 2014-15 disjunction

--- src/otld/combine_appended_files.py
+++ src/otld/combine_appended_files.py
@@ -2,10 +2,52 @@ import os
 
 import pandas as pd
 
-from otld.paths import inter_dir
+from otld.paths import input_dir, inter_dir
+
+
+def get_column_list(crosswalk: pd.DataFrame, column: str | int):
+    columns = crosswalk[column].dropna().to_list()
+    columns = [str(c) for c in columns]
+    columns = ",".join(columns)
+    columns = columns.split(",")
+    columns = [c.strip() for c in columns]
+
+    return columns
+
+
+def map_columns(df: pd.DataFrame, crosswalk_dict: dict):
+    new_df = pd.DataFrame()
+    for revised, original in crosswalk_dict.items():
+        if not original:
+            continue
+        elif isinstance(original, str):
+            new_df[revised] = df[original]
+        elif isinstance(original, list):
+            new_df[revised] = df[original].sum(axis=1)
+
+    return new_df
 
 
 def main():
+    # Load crosswalk
+    crosswalk = pd.read_excel(
+        os.path.join(input_dir, "Instruction Crosswalk.xlsx"), sheet_name="crosswalk"
+    )
+    crosswalk.fillna("", inplace=True)
+    crosswalk = crosswalk.astype(str)
+
+    crosswalk_dict = {}
+    for key, value in zip(crosswalk["196R"], crosswalk[196]):
+        if isinstance(value, str):
+            crosswalk_dict[key] = value.split(",") if value.find(",") > -1 else value
+        elif pd.isna(value) or not value:
+            crosswalk_dict[key] = None
+        else:
+            crosswalk_dict[key] = value
+
+    columns_196 = get_column_list(crosswalk, 196)
+    columns_196_r = get_column_list(crosswalk, "196R")
+
     files = os.listdir(inter_dir)
     federal = []
     state = []
@@ -13,15 +55,15 @@ def main():
 
     # Combine files before 2015
     for file in files:
-        df = pd.read_csv(os.path.join(inter_dir, file))
+        df = pd.read_csv(os.path.join(inter_dir, file), index_col=["STATE", "year"])
         level = "state" if file.startswith("state") else "federal"
 
         if file.find("2015_2023") > -1:
-            post_2015[level] = df
+            post_2015[level] = df.filter(columns_196_r)
         elif level == "federal":
-            federal.append(df)
+            federal.append(df.filter(columns_196))
         elif level == "state":
-            state.append(df)
+            state.append(df.filter(columns_196))
 
     state = pd.concat(state)
     federal = pd.concat(federal)
