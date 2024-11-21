1a4398546d11723e2644d27caf78c2a1cdd76904
RGilliard-Arch <150460521+RGilliard-Arch@users.noreply.github.com>
2024-11-21 09:43:54 -0500

    Refactor crosswalk to include name

--- src/otld/utils/crosswalk_2014_2015.py
+++ src/otld/utils/crosswalk_2014_2015.py
@@ -10,11 +10,14 @@ crosswalk = pd.read_excel(
 crosswalk.fillna("", inplace=True)
 crosswalk = crosswalk.astype(str)
 
-crosswalk_dict = {}
-for key, value in zip(crosswalk["196R"], crosswalk[196]):
-    if isinstance(value, str):
-        crosswalk_dict[key] = value.split(",") if value.find(",") > -1 else value
-    elif pd.isna(value) or not value:
-        crosswalk_dict[key] = None
+crosswalk_dict = crosswalk.set_index(["196R"]).to_dict(orient="index")
+for key in crosswalk_dict:
+    value_196 = crosswalk_dict[key][196]
+    if isinstance(value_196, str):
+        crosswalk_dict[key][196] = (
+            value_196.split(",") if value_196.find(",") > -1 else value_196
+        )
+    elif pd.isna(value_196) or not value_196:
+        crosswalk_dict[key][196] = None
     else:
-        crosswalk_dict[key] = value
+        crosswalk_dict[key][196] = value_196
