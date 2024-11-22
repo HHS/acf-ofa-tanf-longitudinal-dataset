3680e05e50e8d4a957b691ca772037a32af68c1a
RGilliard-Arch <150460521+RGilliard-Arch@users.noreply.github.com>
2024-11-22 14:28:35 -0500

    Refactor append_2010_2014
    - Track column sources
    - Remove SFAG summary sheet
    - Common sheets should only go in the Federal tab

--- src/otld/append_2010_2014.py
+++ src/otld/append_2010_2014.py
@@ -7,7 +7,7 @@ import re
 import openpyxl
 import pandas as pd
 
-from otld.paths import input_dir, inter_dir
+from otld.paths import diagnostics_dir, input_dir, inter_dir
 from otld.utils import (
     convert_to_numeric,
     delete_empty_columns,
@@ -15,9 +15,14 @@ from otld.utils import (
     standardize_line_number,
     validate_data_frame,
 )
+from otld.utils.LineTracker import LineTracker
+
+line_tracker = LineTracker()
 
 
-def rename_columns(df: pd.DataFrame, sheet: str, column_dict: dict) -> pd.DataFrame:
+def rename_columns(
+    df: pd.DataFrame, sheet: str, column_dict: dict, tracker: dict
+) -> pd.DataFrame:
     """Rename TANF dataframe columns
 
     Rename TANF dataframe columns to line numbers.
@@ -34,24 +39,23 @@ def rename_columns(df: pd.DataFrame, sheet: str, column_dict: dict) -> pd.DataFr
         pd.DataFrame: _description_
     """
     # Select the line number for the relevant sheet
-    if sheet == "SFAG Summary":
-        df = df.iloc[:, [0, 1, 2]].copy()
-        df["4"] = df.iloc[:, [1, 2]].sum(axis=1)
-        df = df.iloc[:, [0, 3]]
-        df.columns = ["STATE", "4"]
-        return df
-    elif sheet.endswith("Non-Assistance"):
+    if sheet.endswith("Non-Assistance"):
         number = 6
     elif sheet.endswith("Assistance"):
         number = 5
     elif sheet.endswith("Non-A Subcategories"):
         number = 6
     elif sheet.startswith("Summary"):
-        df = df.iloc[:, [0, 1, 4, 5, 7, 8, 9]]
-        df.columns = ["STATE", "1", "2", "3", "7", "9", "10"]
+        df = df.iloc[:, [0, 1, 2, 4, 5, 7, 8, 9]].copy()
+        tracker["BaseColumns"] = df.columns.to_list()
+        df.columns = ["STATE", "1", "Carryover", "2", "3", "7", "9", "10"]
+        tracker["RenamedColumns"] = df.columns.to_list()
+        df["4"] = df["1"] - df["2"] - df["3"]
         return df
     else:
         raise ValueError("Invalid sheet")
+
+    tracker["BaseColumns"] = df.columns.to_list()
 
     # Select columns associated with the line number
     if sheet.endswith("Subcategories"):
@@ -72,11 +76,17 @@ def rename_columns(df: pd.DataFrame, sheet: str, column_dict: dict) -> pd.DataFr
     columns.extend(column_dict.keys())
     df.columns = [standardize_line_number(column) for column in columns]
 
+    tracker["RenamedColumns"] = df.columns.to_list()
+
     return df
 
 
 def get_tanf_df(
-    tanf_path: str | os.PathLike, sheets: list[str], year: int, column_dict: dict
+    tanf_path: str | os.PathLike,
+    sheets: list[str],
+    year: int,
+    column_dict: dict,
+    level: str,
 ) -> pd.DataFrame:
     """Get data from TANF Financial files
 
@@ -85,6 +95,7 @@ def get_tanf_df(
         sheets (list[str]): List of sheets to extract.
         year (int): The year associated with the Excel workbook.
         column_dict (dict): A dictionary mapping line numbers to field names.
+        level (str): Level of data: "State" or "Federal".
 
     Returns:
         pd.DataFrame: Appended workbooks as data frames.
@@ -92,7 +103,13 @@ def get_tanf_df(
     data = []
     tanf_excel_file = openpyxl.load_workbook(tanf_path)
 
+    line_tracker.sources[year] = []
+    path_stem = os.path.split(tanf_path)[1]
+
     for sheet in sheets:
+        # Begin building tracker dictionary
+        tracker = {"FileName": path_stem, "SheetName": sheet, "Level": level}
+
         # Use openpyxl to get column names and delete columns with no data
         tanf_df = tanf_excel_file[sheet]
         delete_empty_columns(tanf_df)
@@ -101,12 +118,18 @@ def get_tanf_df(
         # Read in data with pandas and rename columns
         tanf_df = pd.read_excel(tanf_path, sheet_name=sheet, skiprows=i, header=None)
         tanf_df.columns = columns
-        tanf_df = rename_columns(tanf_df, sheet, column_dict)
 
-        # Clean up and add to data list
+        tanf_df = rename_columns(tanf_df, sheet, column_dict, tracker)
+        assert len(tracker["BaseColumns"]) == len(tracker["RenamedColumns"])
+
+        # Filter rows
         tanf_df.dropna(subset=["STATE"], inplace=True)
+        tanf_df = tanf_df[~tanf_df["STATE"].str.startswith("Footnote")]
         tanf_df["STATE"] = tanf_df["STATE"].map(lambda x: x.strip())
         tanf_df.set_index("STATE", inplace=True)
+
+        # Append to data list
+        line_tracker.sources[year].append(tracker)
         data.append(tanf_df)
 
     # Concatenate and remove duplicated columns
@@ -151,12 +174,10 @@ def main(export: bool = False) -> tuple[pd.DataFrame]:
     ]
     common_sheets = [
         "Summary Federal Funds",
-        "SFAG Summary",
     ]
     fed_sheets = [f"Federal {sheet}" for sheet in sheets]
     state_sheets = [f"State {sheet}" for sheet in sheets]
     fed_sheets += common_sheets
-    state_sheets += common_sheets
 
     for file in tanf_files:
         # Extract year from file name
@@ -165,11 +186,13 @@ def main(export: bool = False) -> tuple[pd.DataFrame]:
         if year >= 2015:
             continue
 
-        federal_df = get_tanf_df(file.path, fed_sheets, year, column_dict)
-        state_df = get_tanf_df(file.path, state_sheets, year, column_dict)
+        federal_df = get_tanf_df(file.path, fed_sheets, year, column_dict, "Federal")
+        state_df = get_tanf_df(file.path, state_sheets, year, column_dict, "State")
 
         federal.append(federal_df)
         state.append(state_df)
+
+    line_tracker.export(os.path.join(diagnostics_dir, "LineSources.xlsx"))
 
     # Concatenate all years
     federal_df = pd.concat(federal)
