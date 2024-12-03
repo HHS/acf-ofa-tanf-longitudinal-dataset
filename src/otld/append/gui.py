7b87cf025592b47a08325106028c234b5756d1ca
RGilliard-Arch <150460521+RGilliard-Arch@users.noreply.github.com>
2024-12-03 15:17:55 -0500

    Updating tkinter GUI
    - Idea to log error messages to tkinter window
    - Docstrings for functions

--- src/otld/append/gui.py
+++ src/otld/append/gui.py
@@ -1,29 +1,79 @@
 """Gui for updating appended files"""
 
+import time
 import tkinter as tk
 from tkinter import Tk, ttk
-from tkinter.filedialog import askdirectory, askopenfilename
+from tkinter.filedialog import askopenfilename
 
 from otld.append.TANFData import TANFData
 
 
-def browse_dir(entry: ttk.Entry):
-    path = askdirectory()
-    entry.insert(0, path)
-    return
-
-
 def browse_file(entry: ttk.Entry):
+    """Prompt the user to select a file.
+
+    Args:
+        entry (ttk.Entry): A tkinter Entry widget in which to display the selected file.
+    """
     path = askopenfilename()
     entry.insert(0, path)
     return
 
 
+def display_waiting_window(root: Tk):
+    """Update the tkinter window to display a waiting message.
+
+    Args:
+        root (Tk): A root tkinter window.
+    """
+    waiting = ttk.Frame(root, name="waiting")
+    waiting.pack(padx=10, fill="x", expand=True)
+
+    waiting_message = ttk.Label(
+        waiting, text="Please wait, data is being appended.", name="waiting_message"
+    )
+    waiting_message.pack(anchor="center")
+    root.update()
+
+
+def log_to_waiting(cls: type):
+    """Wrapper to log errors to tkinter window.
+
+    Args:
+        cls (type): Class definition.
+    """
+
+    def wrapper(*args, **kwargs):
+        try:
+            value = cls(*args, **kwargs)
+            return value
+        except Exception as e:
+            root = kwargs.get("root")
+            waiting_message = root.children["waiting"].children["waiting_message"]
+            message = e.__str__() + ".\n" + "This window will close in 10 seconds."
+            waiting_message.configure(text=message)
+            root.update()
+            time.sleep(10)
+
+    return wrapper
+
+
+@log_to_waiting
+class TANFData(TANFData):
+    def __init__(self, type: str, appended_path: str, to_append_path: str, root: Tk):
+        super().__init__(type, appended_path, to_append_path)
+
+
 def append_clicked(root: Tk, type: str, appended_path: str, to_append_path: str):
-    print(type, appended_path, to_append_path)
-    tanf_data = TANFData(type, appended_path, to_append_path)
-    if 1 != 1:
+    file_select = root.children["file_select"]
+    file_select.destroy()
+
+    display_waiting_window(root)
+
+    tanf_data = TANFData(type, appended_path, to_append_path, root=root)
+
+    if False:
         tanf_data.append()
+
     root.destroy()
 
 
@@ -33,7 +83,7 @@ def main():
     root.title("Append TANF Data")
     root.geometry("400x250")
 
-    file_select = ttk.Frame(root)
+    file_select = ttk.Frame(root, name="file_select")
     file_select.pack(padx=10, fill="x", expand=True)
 
     # Set string variables for workbook to append and appended workbook
