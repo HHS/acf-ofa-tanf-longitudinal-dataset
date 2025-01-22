5be7164508deb18bac3959e1353e2615a6a7ad8b
RGilliard-Arch <150460521+RGilliard-Arch@users.noreply.github.com>
2025-01-22 09:00:11 -0500

    Separate installations

--- code/generate_documentation.sh
+++ code/generate_documentation.sh
@@ -1,14 +1,12 @@
 # Install packages
 pip install -U pip
 pip install .
-pip install beautifulsoup4 python3-tk
+pip install beautifulsoup4 
+pip install python3-tk
 
 # Remove old documentation and regenerate
 rm -rf documentation/source/_autosummary
 cd documentation
 make html
 
-# Add transformation documentation
 cd ..
-# bash code/make_html_documentation.sh
-# python3 -m code/update_index.py
