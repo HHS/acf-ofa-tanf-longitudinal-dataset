e3b020ad4ee900a4274d7068a478001356ad4954
RGilliard-Arch <150460521+RGilliard-Arch@users.noreply.github.com>
2025-01-24 16:50:28 -0500

    Rewrite pandas utils test in unittest

--- tests/test_pandas_utils.py
+++ tests/test_pandas_utils.py
@@ -1,4 +1,5 @@
 import os
+import unittest
 
 import pandas as pd
 
@@ -6,31 +7,35 @@ import otld.utils.pandas_utils as putils
 from otld.paths import test_dir
 
 
-def test_get_header():
-    df = pd.read_excel(
-        os.path.join(test_dir, "fy2023_ssp_caseload.xlsx"),
-        sheet_name="Avg Month Num Fam Oct 22_Sep 23",
-        header=None,
-    )
+class TestPandasUtils(unittest.TestCase):
+    def test_get_header(self):
+        df = pd.read_excel(
+            os.path.join(test_dir, "fy2023_ssp_caseload.xlsx"),
+            sheet_name="Avg Month Num Fam Oct 22_Sep 23",
+            header=None,
+        )
 
-    # Test returning header row
-    header = putils.get_header(df, 0, "^State$", idx=True)
-    assert header == 5
+        # Test returning header row
+        header = putils.get_header(df, 0, "^State$", idx=True)
+        self.assertEqual(header, 5)
 
-    # Test updating df with appropriate header
-    df = putils.get_header(df)
+        # Test updating df with appropriate header
+        df = putils.get_header(df)
 
-    columns = df.columns.tolist()
-    columns = [col.replace("\n", "").replace(" ", "") for col in columns]
-    columns.sort()
-    assert columns == [
-        "NoParentFamilies",
-        "OneParentFamilies",
-        "State",
-        "TotalFamilies",
-        "TwoParentFamilies",
-    ]
+        columns = df.columns.tolist()
+        columns = [col.replace("\n", "").replace(" ", "") for col in columns]
+        columns.sort()
+        self.assertEqual(
+            columns,
+            [
+                "NoParentFamilies",
+                "OneParentFamilies",
+                "State",
+                "TotalFamilies",
+                "TwoParentFamilies",
+            ],
+        )
 
 
 if __name__ == "__main__":
-    test_get_header()
+    unittest.main()
