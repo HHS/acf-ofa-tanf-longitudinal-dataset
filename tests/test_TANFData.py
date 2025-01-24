885328d74a5ca12fa4d7c77b2a17d2f33386538a
RGilliard-Arch <150460521+RGilliard-Arch@users.noreply.github.com>
2025-01-24 16:41:56 -0500

    Rewrite TANFData tests in unittest

--- tests/test_TANFData.py
+++ tests/test_TANFData.py
@@ -1,57 +1,98 @@
 import os
+import tempfile
 import time
-
-import pytest
+import unittest
 
 from otld.append.TANFData import TANFData
-from otld.paths import scrap_dir
-
-tanf_data = TANFData(
-    "financial",
-    os.path.join(scrap_dir, "FinancialDataWide.xlsx"),
-    os.path.join(scrap_dir, "tanf_financial_data_fy_2024.xlsx"),
-)
+from otld.paths import test_dir
+from otld.utils.MockData import MockData
 
 
-def test_properties():
-    with pytest.raises(AttributeError):
-        tanf_data.appended = 1
-    with pytest.raises(AttributeError):
-        tanf_data.to_append = []
-    with pytest.raises(AttributeError):
-        tanf_data.type = "state"
-    with pytest.raises(AttributeError):
-        tanf_data.sheet_dict = {}
+class TestTANFData(unittest.TestCase):
+    def setUp(self):
+        self.temp_dir = tempfile.TemporaryDirectory()
+        self.mock_dir = self.temp_dir.name
+
+    def test_properties(self):
+        mock_dir = self.mock_dir
+        mock_data = MockData("caseload", 2024)
+        mock_data.generate_data()
+        mock_data.export(dir=mock_dir)
+        mocked_data = [os.path.join(mock_dir, f) for f in os.listdir(mock_dir)]
+
+        tanf_data = TANFData(
+            "caseload",
+            os.path.join(test_dir, "CaseloadDataWide.xlsx"),
+            mocked_data,
+        )
+
+        with self.assertRaises(AttributeError):
+            tanf_data.appended = 1
+
+        with self.assertRaises(AttributeError):
+            tanf_data.to_append = 1
+
+        with self.assertRaises(AttributeError):
+            tanf_data.type = 1
+
+        with self.assertRaises(AttributeError):
+            tanf_data.sheet_dict = 1
+
+        tanf_data.close_excel_files()
+
+    def test_append_caseload(self):
+        mock_dir = self.mock_dir
+        mock_data = MockData("caseload", 2024)
+        mock_data.generate_data()
+        mock_data.export(dir=mock_dir)
+        mocked_data = [os.path.join(mock_dir, f) for f in os.listdir(mock_dir)]
+
+        tanf_data = TANFData(
+            "caseload",
+            os.path.join(test_dir, "CaseloadDataWide.xlsx"),
+            mocked_data,
+        )
+        tanf_data.append()
+
+        current_date = time.strftime("%Y%m%d", time.gmtime())
+        wide_path = os.path.join(test_dir, f"CaseloadDataWide_{current_date}.xlsx")
+        long_path = os.path.join(test_dir, f"CaseloadDataLong_{current_date}.xlsx")
+        self.assertTrue(os.path.exists(wide_path))
+        self.assertTrue(os.path.exists(long_path))
+
+        os.remove(wide_path)
+        os.remove(long_path)
+
+        tanf_data.close_excel_files()
+
+    def test_append_expenditure(self):
+        mock_dir = self.mock_dir
+        mock_data = MockData("financial", 2024)
+        mock_data.generate_data()
+        mock_data.export(dir=mock_dir)
+        mocked_data = [os.path.join(mock_dir, f) for f in os.listdir(mock_dir)]
+        assert len(mocked_data) == 1
+        mocked_data = mocked_data[0]
+
+        tanf_data = TANFData(
+            "financial", os.path.join(test_dir, "FinancialDataWide.xlsx"), mocked_data
+        )
+        tanf_data.append()
+
+        current_date = time.strftime("%Y%m%d", time.gmtime())
+        wide_path = os.path.join(test_dir, f"FinancialDataWide_{current_date}.xlsx")
+        long_path = os.path.join(test_dir, f"FinancialDataLong_{current_date}.xlsx")
+        self.assertTrue(os.path.exists(wide_path))
+        self.assertTrue(os.path.exists(long_path))
+
+        os.remove(wide_path)
+        os.remove(long_path)
+
+        tanf_data.close_excel_files()
+
+    def tearDown(self):
+        return super().tearDown()
 
 
-def test_append_financial():
-    tanf_data.append()
-    current_date = time.strftime("%Y%m%d", time.gmtime())
-    wide_path = os.path.join(scrap_dir, f"FinancialDataWide_{current_date}.xlsx")
-    long_path = os.path.join(scrap_dir, f"FinancialDataLong_{current_date}.xlsx")
-    assert os.path.exists(wide_path)
-    assert os.path.exists(long_path)
-
-    os.remove(wide_path)
-    os.remove(long_path)
-
-
-def test_append_caseload():
-    tanf_data = TANFData(
-        "caseload",
-        os.path.join(scrap_dir, "CaseloadDataWide.xlsx"),
-        [
-            os.path.join(scrap_dir, "fy2024_ssp_caseload.xlsx"),
-            os.path.join(scrap_dir, "fy2024_tanf_caseload.xlsx"),
-            os.path.join(scrap_dir, "fy2024_tanssp_caseload.xlsx"),
-        ],
-    )
-    tanf_data.append()
-    current_date = time.strftime("%Y%m%d", time.gmtime())
-    wide_path = os.path.join(scrap_dir, f"CaseloadDataWide_{current_date}.xlsx")
-    long_path = os.path.join(scrap_dir, f"CaseloadDataLong_{current_date}.xlsx")
-    assert os.path.exists(wide_path)
-    assert os.path.exists(long_path)
-
-    os.remove(wide_path)
-    os.remove(long_path)
+if __name__ == "__main__":
+    unittest.main()
