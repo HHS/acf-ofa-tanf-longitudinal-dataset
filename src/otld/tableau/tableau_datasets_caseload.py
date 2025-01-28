d56b6841bceba42640e35400326bf974febf107b
RGilliard-Arch <150460521+RGilliard-Arch@users.noreply.github.com>
2025-01-28 09:35:07 -0500

    tableau_datasets_caseload: Calculate log adjusted value

--- src/otld/tableau/tableau_datasets_caseload.py
+++ src/otld/tableau/tableau_datasets_caseload.py
@@ -1,13 +1,15 @@
+import math
 import os
 
+import pandas as pd
 from openpyxl.styles.numbers import FORMAT_NUMBER_COMMA_SEPARATED1
 
-from otld.paths import out_dir, tableau_dir
+from otld.paths import tableau_dir
 from otld.utils import excel_to_dict, export_workbook, wide_with_index
 
 
 def generate_wide_data():
-    frames = excel_to_dict(os.path.join(out_dir, "CaseloadDataWideClean.xlsx"))
+    frames = excel_to_dict(os.path.join(tableau_dir, "CaseloadDataWideRaw.xlsx"))
     export_workbook(
         wide_with_index(frames, "CaseloadData"),
         os.path.join(tableau_dir, "data", "CaseloadDataWide.xlsx"),
@@ -19,11 +21,16 @@ def generate_wide_data():
 
 
 def generate_long_data():
-    pass
+    df = pd.read_excel(os.path.join(tableau_dir, "data", "CaseloadDataLongRaw.xlsx"))
+    df["log_value"] = df["Number"].map(
+        lambda x: math.log(x) if isinstance(x, (float, int)) and x != 0 else None
+    )
+    df.to_excel(os.path.join(tableau_dir, "data", "CasleoadDataLong.xlsx"))
 
 
 def main():
     generate_wide_data()
+    generate_long_data()
 
 
 if __name__ == "__main__":
