884d27bf6070447c669f11e0bebebad32bb5b9af
RGilliard-Arch <150460521+RGilliard-Arch@users.noreply.github.com>
2025-01-29 10:49:09 -0500

    Create TANFAppend class
    - Refactor code in append into a class
    - Update tests to function with class
    - Write additional tests for directory function and sheet
    function.

--- tests/test_append.py
+++ tests/test_append.py
@@ -2,13 +2,14 @@ import os
 import sys
 import tempfile
 import unittest
+from unittest.mock import MagicMock
 
-import otld.append.append as append
+from otld.append.append import TANFAppend
 from otld.paths import test_dir
 from otld.utils.MockData import MockData
 
 
-class TestAppend(unittest.TestCase):
+class TestTANFAppend(unittest.TestCase):
     def setUp(self):
         self.temp_dir = tempfile.TemporaryDirectory()
         self.mock_dir = self.temp_dir.name
@@ -20,19 +21,20 @@ class TestAppend(unittest.TestCase):
     def tearDown(self):
         return super().tearDown()
 
-    # def test_main(self):
-    #     sys.argv = [
-    #         "tanf-append",
-    #         "caseload",
-    #         "-b",
-    #         os.path.join(scrap_dir, "CaseloadDataWide.xlsx"),
-    #         "-a",
-    #         os.path.join(scrap_dir, "fy2024_ssp_caseload.xlsx"),
-    #         os.path.join(scrap_dir, "fy2024_tanf_caseload.xlsx"),
-    #         os.path.join(scrap_dir, "fy2024_tanssp_caseload.xlsx"),
-    #     ]
+    def test_append(self):
+        sys.argv = [
+            "tanf-append",
+            "caseload",
+            os.path.join(test_dir, "CaseloadDataWide.xlsx"),
+            "-d",
+            self.mock_dir,
+        ]
 
-    #     append.main()
+        appender = TANFAppend()
+        appender.append = MagicMock()
+        appender.append()
+
+        appender.append.assert_called_once()
 
     def test_get_files(self):
         with open(os.path.join(self.mock_dir, "caseload.txt"), "w") as f:
@@ -49,8 +51,8 @@ class TestAppend(unittest.TestCase):
             self.mock_dir,
         ]
 
-        parser = append.parse_args(sys.argv[1:])
-        files = append.get_files(parser.directory)
+        appender = TANFAppend()
+        files = appender._to_append
         files = [file.split("\\")[-1] for file in files]
         files.sort()
         correct = [
@@ -61,9 +63,109 @@ class TestAppend(unittest.TestCase):
         correct.sort()
         self.assertEqual(correct, files)
 
+    def test_get_sheets(self):
+        # Check that the correct sheets are obtained, financial
+        sys.argv = [
+            "tanf-append",
+            "financial",
+            os.path.join(test_dir, "CaseloadDataWide.xlsx"),
+            "-d",
+            self.mock_dir,
+            "-s",
+            '{"financial": {"Total": "B. Total Expenditures", "Federal": "C.1 Federal Expenditures", "State": "C.2 State Expenditures"}}',
+        ]
+        appender = TANFAppend()
+        correct = {
+            "financial": {
+                "Total": "B. Total Expenditures",
+                "Federal": "C.1 Federal Expenditures",
+                "State": "C.2 State Expenditures",
+            }
+        }
+        self.assertEqual(correct, appender._sheets)
+
+        # Check that the correct sheets are obtained, caseload
+        sys.argv = [
+            "tanf-append",
+            "caseload",
+            os.path.join(test_dir, "CaseloadDataWide.xlsx"),
+            "-d",
+            self.mock_dir,
+            "-s",
+            """{"caseload": {
+                "TANF": {"family": "fycy2024-families", "recipient": "fycy2024-recipients"}, 
+                "SSP_MOE": {"family": "Avg Month Num Fam", "recipient": "Avg Mo. Num Recipient"},
+                "TANF_SSP": {"family": "fycy2024-families", "recipient": "Avg Mo. Num Recipient"}
+            }}""",
+        ]
+        correct = {
+            "caseload": {
+                "TANF": {
+                    "family": "fycy2024-families",
+                    "recipient": "fycy2024-recipients",
+                },
+                "SSP_MOE": {
+                    "family": "Avg Month Num Fam",
+                    "recipient": "Avg Mo. Num Recipient",
+                },
+                "TANF_SSP": {
+                    "family": "fycy2024-families",
+                    "recipient": "Avg Mo. Num Recipient",
+                },
+            }
+        }
+        appender = TANFAppend()
+        self.assertEqual(correct, appender._sheets)
+
+        # Confirm error is raised if Total, Federal, or State is missing
+        sys.argv = [
+            "tanf-append",
+            "financial",
+            os.path.join(test_dir, "CaseloadDataWide.xlsx"),
+            "-d",
+            self.mock_dir,
+            "-s",
+            '{"financial": {"Total": "B. Total Expenditures", "Federal": "C.1 Federal Expenditures"}}',
+        ]
+        with self.assertRaises(AssertionError):
+            TANFAppend()
+
+        # Confirm error is raised if TANF, TANF_SSP, or SSP_MOE is missing
+        sys.argv = [
+            "tanf-append",
+            "caseload",
+            os.path.join(test_dir, "CaseloadDataWide.xlsx"),
+            "-d",
+            self.mock_dir,
+            "-s",
+            """{"caseload": {
+                "TANF": {"family": "fycy2024-families", "recipient": "fycy2024-recipients"}, 
+                "TANF_SSP": {"family": "fycy2024-families", "recipient": "Avg Mo. Num Recipient"}
+            }}""",
+        ]
+        with self.assertRaises(AssertionError):
+            TANFAppend()
+
+        # Assert error if family or recipient missing from keys
+        sys.argv = [
+            "tanf-append",
+            "caseload",
+            os.path.join(test_dir, "CaseloadDataWide.xlsx"),
+            "-d",
+            self.mock_dir,
+            "-s",
+            """{"caseload": {
+                "TANF": {"family": "fycy2024-families", "recipient": "fycy2024-recipients"}, 
+                "SSP_MOE": {"family": "Avg Month Num Fam", "recipient": "Avg Mo. Num Recipient"},
+                "TANF_SSP": {"family": "fycy2024-families"}
+            }}""",
+        ]
+        with self.assertRaises(AssertionError):
+            TANFAppend()
+
 
 if __name__ == "__main__":
     # unittest.main()
     suite = unittest.TestSuite()
-    suite.addTest(TestAppend("test_get_files"))
+    suite.addTest(TestTANFAppend("test_append"))
     unittest.TextTestRunner().run(suite)
