e51f72767e95a0b241cad37bec8cff46423cdc31
RGilliard-Arch <150460521+RGilliard-Arch@users.noreply.github.com>
2025-01-31 09:52:48 -0500

    Update long tableau caseload file

--- src/otld/tableau/tableau_datasets_caseload.py
+++ src/otld/tableau/tableau_datasets_caseload.py
@@ -1,6 +1,9 @@
+"""Module for caseload Tableau-specific datasets"""
+
 import math
 import os
 
+import numpy as np
 import pandas as pd
 from openpyxl.styles.numbers import FORMAT_NUMBER_COMMA_SEPARATED1
 
@@ -9,6 +12,7 @@ from otld.utils import excel_to_dict, export_workbook, wide_with_index
 
 
 def generate_wide_data():
+    """Generate Tableau-specific wide dataset"""
     frames = excel_to_dict(
         os.path.join(tableau_dir, "data", "CaseloadDataWideRaw.xlsx")
     )
@@ -23,10 +27,48 @@ def generate_wide_data():
 
 
 def generate_long_data():
+    """Generate Tableau-specific long dataset"""
     df = pd.read_excel(os.path.join(tableau_dir, "data", "CaseloadDataLongRaw.xlsx"))
+
     df["log_value"] = df["Number"].map(
         lambda x: math.log(x) if isinstance(x, (float, int)) and x != 0 else None
     )
+
+    # Calculate percentage of total
+    df["group"] = df["Category"].apply(
+        lambda x: "family" if x.lower().find("fam") > -1 else "recipients"
+    )
+    total = df[df["Category"].apply(lambda x: x.lower().find("total") > -1)]
+    total = total[["FiscalYear", "State", "Funding", "group", "Number"]].rename(
+        columns={"Number": "Total"}
+    )
+    df = df.merge(total, how="left", on=["FiscalYear", "State", "Funding", "group"])
+    df["pct_of_total"] = round(df["Number"] / df["Total"], 4) * 100
+    df["pct_of_total"] = df["pct_of_total"].replace(
+        [np.nan, np.inf, -np.inf], [0, 0, 0]
+    )
+    df.drop("Total", inplace=True, axis=1)
+
+    # Deviation from base year
+    base_year = (
+        df.sort_values(["FiscalYear", "State", "Funding", "Category"])
+        .groupby(["State", "Funding", "Category"])
+        .first()[["FiscalYear", "Number"]]
+        .rename(columns={"Number": "base"})
+        .reset_index()
+    )
+    # Confirm years are as expected
+    assert all([year in [2000, 1997] for year in base_year["FiscalYear"]])
+    base_year.drop("FiscalYear", axis=1, inplace=True)
+
+    df = df.merge(base_year, how="left", on=["State", "Funding", "Category"])
+    df["pct_deviation"] = round(df["Number"] / df["base"], 4) * 100
+    df["pct_deviation"] = df["pct_deviation"].replace(
+        [np.nan, np.inf, -np.inf], [0, 0, 0]
+    )
+    df.drop("base", inplace=True, axis=1)
+
+    # Export
     df.to_excel(
         os.path.join(tableau_dir, "data", "CasleoadDataLong.xlsx"),
         sheet_name="CaseloadData",
