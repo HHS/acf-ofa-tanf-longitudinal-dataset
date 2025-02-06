12ac81eb1960ba5930fb97486a687f6d8d810854
RGilliard-Arch <150460521+RGilliard-Arch@users.noreply.github.com>
2025-02-06 16:39:18 -0500

    Add two additional tests for command line applications

--- tests/integration/test_otld.py
+++ tests/integration/test_otld.py
@@ -7,18 +7,6 @@ import pytest
 
 from otld.utils.MockData import MockData
 
-# Tests with different command line argument
-# See documentation for tempfile fixture
-
-# Tests needed
-# Appending:
-# 1) Submit a directory
-# 2) Submit a list of files
-# 3) Submit a dictionary of sheets with either 1 or 2
-# Tableau:
-# 1) Generate caseload
-# 2) Generate financial
-
 # Paths
 ROOT = os.path.join(os.path.dirname(__file__), "..", "..")
 DATA_DIR = os.path.join(ROOT, "data")
@@ -30,8 +18,8 @@ CURRENT_DATE = time.strftime("%Y%m%d", time.gmtime())
 # Fixtures
 @pytest.fixture(scope="class")
 def mock_data(request, tmp_path_factory):
+    # Set locals
     root = tmp_path_factory.mktemp("test")
-
     kind = request.param
     file = f"{kind.title()}DataWide.xlsx"
 
@@ -45,16 +33,18 @@ def mock_data(request, tmp_path_factory):
     tableau = root / "tableau"
     tableau.mkdir()
 
-    MockData(kind, 2024).generate_data().export(to_append)
-
-    shutil.copy(os.path.join(APPENDED, file), os.path.join(appended, file))
-    shutil.copy(os.path.join(MISC, "pce.csv"), os.path.join(tableau, "pce.csv"))
-
     directories = {
         directory: eval(directory)
         for directory in ["root", "to_append", "appended", "tableau"]
     }
 
+    # Generate mock data and move real data
+    MockData(kind, 2024).generate_data().export(to_append)
+
+    shutil.copy(os.path.join(APPENDED, file), os.path.join(appended, file))
+    shutil.copy(os.path.join(MISC, "pce.csv"), os.path.join(tableau, "pce.csv"))
+
+    # Set class attributes
     request.cls._mock_dir_dict = directories
     request.cls._kind = kind
     request.cls._file = file
@@ -88,6 +78,94 @@ class TestDirectoryOTLD:
                 self._mock_dir_dict["appended"] / self._file,
                 "-d",
                 self._mock_dir_dict["to_append"],
+            ]
+        )
+
+        assert_appended_exists(self._mock_dir_dict["appended"], self._kind)
+
+    def test_tableau(self):
+        arguments = [
+            "tanf-tableau",
+            self._kind,
+            self._mock_dir_dict["appended"]
+            / f"{self._kind}DataWide_{CURRENT_DATE}.xlsx",
+            self._mock_dir_dict["tableau"],
+        ]
+        if self._kind == "financial":
+            arguments += ["-i", self._mock_dir_dict["tableau"] / "pce.csv"]
+
+        subprocess.run(arguments)
+
+        assert_tableau_exists(self._mock_dir_dict["tableau"], self._kind)
+
+
+@pytest.mark.incremental
+@pytest.mark.parametrize("mock_data", ["caseload", "financial"], indirect=True)
+@pytest.mark.usefixtures("mock_data")
+class TestListFilesOTLD:
+    def test_append(self):
+        subprocess.run(
+            [
+                "tanf-append",
+                self._kind,
+                self._mock_dir_dict["appended"] / self._file,
+                "-a",
+                *[
+                    os.path.join(self._mock_dir_dict["to_append"], file)
+                    for file in os.listdir(self._mock_dir_dict["to_append"])
+                ],
+            ]
+        )
+
+        assert_appended_exists(self._mock_dir_dict["appended"], self._kind)
+
+    def test_tableau(self):
+        arguments = [
+            "tanf-tableau",
+            self._kind,
+            self._mock_dir_dict["appended"]
+            / f"{self._kind}DataWide_{CURRENT_DATE}.xlsx",
+            self._mock_dir_dict["tableau"],
+        ]
+        if self._kind == "financial":
+            arguments += ["-i", self._mock_dir_dict["tableau"] / "pce.csv"]
+
+        subprocess.run(arguments)
+
+        assert_tableau_exists(self._mock_dir_dict["tableau"], self._kind)
+
+
+@pytest.mark.incremental
+@pytest.mark.parametrize("mock_data", ["caseload", "financial"], indirect=True)
+@pytest.mark.usefixtures("mock_data")
+class TestSheetsOTLD:
+    def test_append(self):
+        if self._kind == "financial":
+            sheets = """
+            {
+                "financial": {"Total": "B. Total Expenditures", "Federal": "C.1 Federal Expenditures", "State": "C.2 State Expenditures"}
+            }
+            """
+        elif self._kind == "caseload":
+            sheets = """
+            {
+                "caseload": {
+                    "TANF": {"family": "fycy2024-families", "recipient": "fycy2024-recipients"}, 
+                    "SSP_MOE": {"family": "Avg Month Num Fam", "recipient": "Avg Mo. Num Recipient"}, 
+                    "TANF_SSP": {"family": "fycy2024-families", "recipient": "Avg Mo. Num Recipient"}
+                }
+            }
+            """
+
+        subprocess.run(
+            [
+                "tanf-append",
+                self._kind,
+                self._mock_dir_dict["appended"] / self._file,
+                "-d",
+                self._mock_dir_dict["to_append"],
+                "-s",
+                sheets,
             ]
         )
 
